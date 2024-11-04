159fae66a0cd7e9a8dd14c644b01e9c8c3c8ae37
nayoung <nayoung021008@naver.com>
2024-11-04 20:02:03 +0900

    design: landing,login,meeting page

--- timeTogether/src/pages/LandingPage.jsx
+++ timeTogether/src/pages/LandingPage.jsx
@@ -11,11 +11,11 @@ function LandingPage() {
     // 1초 후 전환 애니메이션 시작
     const swapTimeout = setTimeout(() => {
       setIsSwapped(true); // 상태 전환
-    }, 900);
+    }, 1000);
 
     const navigateTimeout = setTimeout(() => {
       navigate("/login");
-    }, 1800);
+    }, 2000);
 
     return () => clearTimeout(swapTimeout); // 타이머 정리
   }, []);
