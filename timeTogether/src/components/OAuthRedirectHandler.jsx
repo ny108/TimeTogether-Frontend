ee3476e38c3956378c28927090f81eb3c63f5b64
nayoung <nayoung021008@naver.com>
2024-11-07 02:45:09 +0900

    feat: 로그인 연결관련 코드수정

--- timeTogether/src/components/OAuthRedirectHandler.jsx
+++ timeTogether/src/components/OAuthRedirectHandler.jsx
@@ -1,8 +1,44 @@
-// OAuthRedirectHandler.jsx
+// // OAuthRedirectHandler.jsx
+// import React, { useEffect } from "react";
+// import { useHistory } from "react-router-dom";
+
+// // 로컬 스토리지에 저장하는 함수
+// function saveTokenToLocalStorage(accessToken, refreshToken) {
+//   localStorage.setItem("accessToken", accessToken);
+//   localStorage.setItem("refreshToken", refreshToken);
+// }
+
+// function OAuthRedirectHandler() {
+//   const history = useHistory();
+
+//   useEffect(() => {
+//     // URL 파라미터에서 토큰 추출
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const accessToken = urlParams.get("accessToken");
+//     const refreshToken = urlParams.get("refreshToken");
+
+//     if (accessToken && refreshToken) {
+//       // 로컬 스토리지에 토큰 저장
+//       saveTokenToLocalStorage(accessToken, refreshToken);
+
+//       // 메인 페이지로 리다이렉트
+//       history.push("/");
+//     } else {
+//       console.error("토큰이 없습니다.");
+//       history.push("/login"); // 토큰이 없으면 로그인 페이지로 이동
+//     }
+//   }, [history]);
+
+//   return <div>로그인 처리 중...</div>;
+// }
+
+// export default OAuthRedirectHandler;
+
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
-// 로컬 스토리지에 저장하는 함수
+// 로컬 스토리지에 토큰 저장
 function saveTokenToLocalStorage(accessToken, refreshToken) {
   localStorage.setItem("accessToken", accessToken);
   localStorage.setItem("refreshToken", refreshToken);
@@ -12,21 +48,30 @@ function OAuthRedirectHandler() {
   const history = useHistory();
 
   useEffect(() => {
-    // URL 파라미터에서 토큰 추출
-    const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get("accessToken");
-    const refreshToken = urlParams.get("refreshToken");
+    // 백엔드에서 토큰을 가져오는 추가 요청
+    axios
+      .get("http://백엔드_IP주소:8080/get-token", {
+        withCredentials: true, // 자격 증명 정보 포함(CORS)
+      })
+      .then((response) => {
+        const accessToken = response.data.accessToken;
+        const refreshToken = response.data.refreshToken;
 
-    if (accessToken && refreshToken) {
-      // 로컬 스토리지에 토큰 저장
-      saveTokenToLocalStorage(accessToken, refreshToken);
+        if (accessToken && refreshToken) {
+          // 로컬 스토리지에 토큰 저장
+          saveTokenToLocalStorage(accessToken, refreshToken);
 
-      // 메인 페이지로 리다이렉트
-      history.push("/");
-    } else {
-      console.error("토큰이 없습니다.");
-      history.push("/login"); // 토큰이 없으면 로그인 페이지로 이동
-    }
+          // 로그인 성공 시 메인 페이지로 이동
+          history.push("/group");
+        } else {
+          console.error("토큰이 없습니다.");
+          history.push("/login"); // 토큰이 없으면 로그인 페이지로 이동
+        }
+      })
+      .catch((error) => {
+        console.error("토큰을 가져오는 데 실패했습니다.", error);
+        history.push("/login"); // 오류 시 로그인 페이지로 이동
+      });
   }, [history]);
 
   return <div>로그인 처리 중...</div>;
--- timeTogether/src/pages/Login.jsx
+++ timeTogether/src/pages/Login.jsx
@@ -1,44 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-// import "./Login.css"; // css파일 import
-// import logo from "../img/logo.png"; // 이미지 가져오기
-// import logo2 from "../img/logo_purple.png";
-// import kakao from "../img/kakao.png";
-// import naver from "../img/naver.png";
-// import google from "../img/google.png";
-
-// function Login() {
-//   return (
-//     <div className="login-container">
-//       {/* 로고 */}
-//       <Link to="/group">
-//         <img className="login-logo" src={logo} alt="로고"></img>
-//       </Link>
-//       {/* 앱 이름 */}
-//       <h1 className="login-title">타임투게더</h1>
-
-//       {/* 앱 설명 */}
-//       <p className="login-description">쉽고 빠른 약속 생성 도우미</p>
-
-//       {/* 소셜 로그인 */}
-//       <div className="social-login-container">
-//         <p className="social-login-title">소셜 로그인</p>
-//         <a href="/oauth2/authorization/naver">
-//           <img className="social-button" src={naver} alt="네이버로 시작"></img>
-//         </a>
-//         <a href="/oauth2/authorization/kakao">
-//           <img className="social-button" src={kakao} alt="카카오로 시작"></img>
-//         </a>
-//         <a href="/oauth2/authorization/google">
-//           <img className="social-button" src={google} alt="구글로 시작"></img>
-//         </a>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Login;
-
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
@@ -48,33 +7,6 @@ import naver from "../img/naver.png";
 import google from "../img/google.png";
 
 function Login() {
-  const handleLogin = async (provider) => {
-    try {
-      const response = await fetch(
-        `http://192.168.86.49:8080/oauth2/authorization/${provider}`,
-        {
-          method: "GET",
-          credentials: "include",
-        }
-      );
-
-      if (response.ok) {
-        // 헤더에서 토큰 추출
-        const token = response.headers.get("Authorization"); // 토큰을 담고 있는 헤더 키로 수정
-        if (token) {
-          localStorage.setItem("token", token); // 로컬 스토리지에 저장
-          window.location.href = "/group"; // 로그인 성공 시 리다이렉트
-        } else {
-          console.error("토큰을 찾을 수 없습니다.");
-        }
-      } else {
-        console.error("로그인 실패");
-      }
-    } catch (error) {
-      console.error("로그인 요청 중 오류 발생:", error);
-    }
-  };
-
   return (
     <div className="login-container">
       <Link to="/group">
@@ -85,15 +17,18 @@ function Login() {
 
       <div className="social-login-container">
         <p className="social-login-title">소셜 로그인</p>
-        <button onClick={() => handleLogin("naver")}>
+        {/* 백엔드의 로그인 인증 엔드포인트로 리다이렉트 (IP주소, localhost번호 확인 필요) */}
+        <a href="http://192.168.86.49:8080/oauth2/authorization/naver">
           <img className="social-button" src={naver} alt="네이버로 시작" />
-        </button>
-        <button onClick={() => handleLogin("kakao")}>
+        </a>
+
+        <a href="http://192.168.86.49:8080/oauth2/authorization/kakao">
           <img className="social-button" src={kakao} alt="카카오로 시작" />
-        </button>
-        <button onClick={() => handleLogin("google")}>
+        </a>
+
+        <a href="http://192.168.86.49:8080/oauth2/authorization/google">
           <img className="social-button" src={google} alt="구글로 시작" />
-        </button>
+        </a>
       </div>
     </div>
   );
